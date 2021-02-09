523e73f7ce6c70ed1f64e97b30a84c8a816355e5
CGuthrieQA <CGuthrie@QA.com>
2021-02-09 16:23:05 +0000

    added lombok

--- src/main/java/com/qa/barn/persistance/domain/Animal.java
+++ src/main/java/com/qa/barn/persistance/domain/Animal.java
@@ -8,6 +8,11 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 @Entity
 public class Animal {
 
@@ -29,9 +34,9 @@ public class Animal {
 	@JoinColumn(name = "barn_id", nullable = false)
 	private Barn barn = null;
 
-	public Animal() {
-		super();
-	}
+//	public Animal() {
+//		super();
+//	}
 
 	public Animal(@NotNull String type, @NotNull int age, @NotNull String name) {
 		super();
@@ -48,95 +53,95 @@ public class Animal {
 		this.name = name;
 	}
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Barn getBarn() {
-		return barn;
-	}
-
-	public void setBarn(Barn barn) {
-		this.barn = barn;
-	}
-
-	@Override
-	public String toString() {
-		return String.format("Animal [id=%s, type=%s, age=%s, name=%s, barn=%s]", id, type, age, name, barn);
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + age;
-		result = prime * result + ((barn == null) ? 0 : barn.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((type == null) ? 0 : type.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Animal other = (Animal) obj;
-		if (age != other.age)
-			return false;
-		if (barn == null) {
-			if (other.barn != null)
-				return false;
-		} else if (!barn.equals(other.barn))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (type == null) {
-			if (other.type != null)
-				return false;
-		} else if (!type.equals(other.type))
-			return false;
-		return true;
-	}
+//	public Long getId() {
+//		return id;
+//	}
+//
+//	public void setId(Long id) {
+//		this.id = id;
+//	}
+//
+//	public String getType() {
+//		return type;
+//	}
+//
+//	public void setType(String type) {
+//		this.type = type;
+//	}
+//
+//	public int getAge() {
+//		return age;
+//	}
+//
+//	public void setAge(int age) {
+//		this.age = age;
+//	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//	public Barn getBarn() {
+//		return barn;
+//	}
+//
+//	public void setBarn(Barn barn) {
+//		this.barn = barn;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return String.format("Animal [id=%s, type=%s, age=%s, name=%s, barn=%s]", id, type, age, name, barn);
+//	}
+//
+//	@Override
+//	public int hashCode() {
+//		final int prime = 31;
+//		int result = 1;
+//		result = prime * result + age;
+//		result = prime * result + ((barn == null) ? 0 : barn.hashCode());
+//		result = prime * result + ((id == null) ? 0 : id.hashCode());
+//		result = prime * result + ((name == null) ? 0 : name.hashCode());
+//		result = prime * result + ((type == null) ? 0 : type.hashCode());
+//		return result;
+//	}
+//
+//	@Override
+//	public boolean equals(Object obj) {
+//		if (this == obj)
+//			return true;
+//		if (obj == null)
+//			return false;
+//		if (getClass() != obj.getClass())
+//			return false;
+//		Animal other = (Animal) obj;
+//		if (age != other.age)
+//			return false;
+//		if (barn == null) {
+//			if (other.barn != null)
+//				return false;
+//		} else if (!barn.equals(other.barn))
+//			return false;
+//		if (id == null) {
+//			if (other.id != null)
+//				return false;
+//		} else if (!id.equals(other.id))
+//			return false;
+//		if (name == null) {
+//			if (other.name != null)
+//				return false;
+//		} else if (!name.equals(other.name))
+//			return false;
+//		if (type == null) {
+//			if (other.type != null)
+//				return false;
+//		} else if (!type.equals(other.type))
+//			return false;
+//		return true;
+//	}
 
 }
